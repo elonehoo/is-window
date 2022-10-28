416261d535dd3b233aae99e9032cf34357ceead4
elonehoo <huchengyea@163.com>
2022-10-28 13:54:23 +0800

    test:complete the basic test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,15 @@
 import { describe, expect, it } from 'vitest'
+import isWindow from '../src/index'
 
 describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+  it('is-window', () => {
+    expect(isWindow(1)).toEqual(false)
+    expect(isWindow('1')).toEqual(false)
+    expect(isWindow(true)).toEqual(false)
+    expect(isWindow(null)).toEqual(false)
+    expect(isWindow({})).toEqual(false)
+    expect(isWindow(function(){})).toEqual(false)
+    expect(isWindow([])).toEqual(false)
   })
 })
+
